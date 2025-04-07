4d8858e1661bc1ac1de3350a020bc0115c51d0eb
Mehakpreet-5 <themehakk@gmail.com>
2025-04-07 22:15:05 +0530

    Update vite.config.js

--- ff/my-project/vite.config.js
+++ ff/my-project/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   //  base: '/', 
   plugins: [react()],
   server: {
-    host: 'localhost',  // Ensure it's accessible locally
+    host: '0.0.0.0',  // Ensure it's accessible locally
     port: 3000,         // Change to desired port
     strictPort: true,   // Ensures Vite fails if the port is unavailable
   },
