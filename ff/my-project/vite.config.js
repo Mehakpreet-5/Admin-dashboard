59766b495b334f0d4b72522dbd9448c26b36d28c
Mehakpreet-5 <themehakk@gmail.com>
2025-04-07 09:12:22 +0530

    Update vite.config.js

--- ff/my-project/vite.config.js
+++ ff/my-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './', // ✅ Add this line
   plugins: [react()],
   server: {
     host: 'localhost',  // Ensure it's accessible locally
